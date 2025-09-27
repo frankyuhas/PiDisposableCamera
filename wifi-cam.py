a259ebc176f0b9f7e958be6f0439c639ec3a94f9
Frank Yuhas <frankyuhas@icloud.com>
2025-09-27 15:41:00 -0400

    Wifi cam code

---
+++ wifi-cam.py
@@ -0,0 +1,20 @@
+from picamera2 import Picamera2, Preview
+from picamera2.encoders import MJPEGEncoder
+from picamera2.outputs import FileOutput
+import io
+
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration()
+picam2.configure(video_config)
+
+# Start an MJPEG HTTP server on port 8000
+picam2.start_recording(MJPEGEncoder(), FileOutput("server:8000"))
+
+try:
+    print("Camera stream running at http://<raspberrypi-ip>:8000")
+    while True:
+        pass  # keep alive
+except KeyboardInterrupt:
+    pass
+finally:
+    picam2.stop_recording()
